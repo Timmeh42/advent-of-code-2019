064c8358207a5dc7148bf013f4cf8354b875982d
Timmeh42 <tmeier42@gmail.com>
2019-12-02 12:01:53 +0200

    part 2

--- day 2/solution.js
+++ day 2/solution.js
@@ -2,30 +2,34 @@ const fs = require('fs');
 
 const rawInput = fs.readFileSync('./input.txt', { encoding: 'utf8', });
 
-let input = rawInput.split(',').map(x => parseInt(x));
+let baseInput = rawInput.split(',').map(x => parseInt(x));
 
-input[1] = 12;
-input[2] = 2;
+for (let a = 0; a < 100; a++) {
+    for (let b = 0; b < 100; b++) {
+        let input = [...baseInput];
 
-//input = [1,1,1,4,99,5,6,0,99];
+        input[1] = a;
+        input[2] = b;
 
-
-let i = 0;
-while (true) {
-    const x = input[i + 1];
-    const y = input[i + 2];
-    const output = input[i + 3];
-
-    if (input[i] === 1) {
-        input[output] = input[x] + input[y];
-    } else if (input[i] === 2) {
-        input[output] = input[x] * input[y];
-    } else if (input[i] === 99) {
-        console.log('end', input[0]);
-        break;
-    } else {
-        console.log('REEEEEE', input[i]);
-        break;
+        let i = 0;
+        while (true) {
+            const x = input[i + 1];
+            const y = input[i + 2];
+            const output = input[i + 3];
+        
+            if (input[i] === 1) {
+                input[output] = input[x] + input[y];
+            } else if (input[i] === 2) {
+                input[output] = input[x] * input[y];
+            } else if (input[i] === 99) {
+                break;
+            } else {
+                break;
+            }
+            i += 4;
+        }
+        if (input[0] === 19690720) {
+            console.log(100 * a + b);
+        }
     }
-    i += 4;
 }
