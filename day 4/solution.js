db20707cffda37892b81c011f4243a7a2419de5d
Tim <tmeier42+github@gmail.com>
2019-12-04 23:36:58 +0200

    handle any range

--- day 4/solution.js
+++ day 4/solution.js
@@ -1,28 +1,40 @@
 const fs = require('fs');
-
+const { performance } = require('perf_hooks');
 let [ min, max ] = fs.readFileSync('./input.txt', { encoding: 'utf8', }).split('-').map(n => Number.parseInt(n));
 
-let startTime = process.hrtime();
+min = 0;
+max = 9999999999;
+
+let [count1, count2] = [0, 0];
+
+let startTime = performance.now();
 
 function checkViable (password) {
     let prevdigit = 0;
     let spree = 0;
+    let true1 = 0;
+    let true2 = 0;
     while (true) {
         let digit = password % 10;
         if (digit === prevdigit) {
+            true1 = 1;
             spree++;
         } else {
             if (spree == 1) {
-                return true;
+                true2 = 1;
+                break;
             }
             spree = 0;
         }
         prevdigit = digit;
         if (password < 10) {
-            return spree === 1;
+            true2 = spree === 1;
+            break;
         }
         password = Math.floor(password / 10);
     }
+    count1 += true1;
+    count2 += true2;
 }
 
 let passwords = [];
@@ -31,9 +43,8 @@ function password (current) {
     if (current > max) {
         return;
     }
-    if (current >= min && checkViable(current)) {
-        passwords.push(current);
-        return;
+    if (current >= min) {
+        checkViable(current);
     }
     let prevDigit = (current % 10) || 1;
     for (let i = prevDigit; i < 10; i++) {
@@ -41,5 +52,5 @@ function password (current) {
     }
 }
 password(0);
-console.log(process.hrtime(startTime));
-console.log(passwords.length);
+console.log(performance.now() - startTime);
+console.log(count1, count2);
