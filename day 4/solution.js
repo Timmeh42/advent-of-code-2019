b6e2e32be3ce80078308b1535a64afa868962ec6
Tim <tmeier42+github@gmail.com>
2019-12-04 23:09:46 +0200

    a faster part 2

--- day 4/solution.js
+++ day 4/solution.js
@@ -6,14 +6,20 @@ let startTime = process.hrtime();
 
 function checkViable (password) {
     let prevdigit = 0;
+    let spree = 0;
     while (true) {
         let digit = password % 10;
         if (digit === prevdigit) {
-            return true;
+            spree++;
+        } else {
+            if (spree == 1) {
+                return true;
+            }
+            spree = 0;
         }
         prevdigit = digit;
         if (password < 10) {
-            return false;
+            return spree === 1;
         }
         password = Math.floor(password / 10);
     }
