d29c49a18747947f0b4cae6631860d8d0c11fa6c
Tim <tmeier42+github@gmail.com>
2019-12-05 12:35:30 +0200

    part 2

--- day 5/solution.js
+++ day 5/solution.js
@@ -5,13 +5,17 @@ const input = fs.readFileSync('./input.txt', { encoding: 'utf8', });
 const codes = input.split(',').map(n => parseInt(n));
 
 let pointer = 0;
-const inputVar = 1;
+const inputVar = 5;
 
 let ins = new Map([
     [1, add],
     [2, mul],
     [3, inp],
     [4, out],
+    [5, jit],
+    [6, jif],
+    [7, lt],
+    [8, eq],
     [99, end],
 ]);
 
@@ -75,11 +79,95 @@ function out (modes) {
     } else {
         console.log(`${pointer}: ${codes[pointer]} | return ${codes[pointer]}`);
     }
-    return modes[0] === 1 ? codes[pointer] : codes[codes[pointer]];
+    return true;
 }
 
 function end (modes) {
     return false;
+}
+
+function jit (modes) {
+    pointer++;
+    let choice = false;
+    if (modes[0] === 0) {
+        console.log(`${pointer}: ${codes[pointer]} | jump if codes[${codes[pointer]}] = ${codes[codes[pointer]]} is not 0`);
+        choice = codes[codes[pointer]] !== 0;
+    } else {
+        console.log(`${pointer}: ${codes[pointer]} | jump if ${codes[pointer]} is not 0`);
+        choice = codes[pointer] !== 0;
+    }
+
+    pointer++;
+    if (choice) {
+        return modes[1] === 1 ? codes[pointer] : codes[codes[pointer]];
+    } else {
+        return true;
+    }
+}
+
+function jif (modes) {
+    pointer++;
+    let choice = false;
+    if (modes[0] === 0) {
+        console.log(`${pointer}: ${codes[pointer]} | jump if codes[${codes[pointer]}] = ${codes[codes[pointer]]} is 0`);
+        choice = codes[codes[pointer]] === 0;
+    } else {
+        console.log(`${pointer}: ${codes[pointer]} | jump if ${codes[pointer]} is 0`);
+        choice = codes[pointer] === 0;
+    }
+
+    pointer++;
+    if (choice) {
+        return modes[1] === 1 ? codes[pointer] : codes[codes[pointer]];
+    } else {
+        return true;
+    }
+}
+
+function lt (modes) {
+    pointer++;
+    const a = modes[0] ? codes[pointer] : codes[codes[pointer]];
+    if (modes[0] === 0) {
+        console.log(`${pointer}: ${codes[pointer]} | a = codes[${codes[pointer]}] = ${a}`);
+    } else {
+        console.log(`${pointer}: ${codes[pointer]} | a = ${codes[pointer]}`);
+    }
+    
+    pointer++;
+    const b = modes[1] ? codes[pointer] : codes[codes[pointer]];
+    if (modes[1] === 0) {
+        console.log(`${pointer}: ${codes[pointer]} | b = codes[${codes[pointer]}] = ${b}`);
+    } else {
+        console.log(`${pointer}: ${codes[pointer]} | b = ${codes[pointer]}`);
+    }
+
+    pointer++;
+    codes[codes[pointer]] = a < b ? 1 : 0;
+    console.log(`${pointer}: ${codes[pointer]} | store a < b = ${a < b} in ${codes[pointer]}`);
+    return true;
+}
+
+function eq (modes) {
+    pointer++;
+    const a = modes[0] ? codes[pointer] : codes[codes[pointer]];
+    if (modes[0] === 0) {
+        console.log(`${pointer}: ${codes[pointer]} | a = codes[${codes[pointer]}] = ${a}`);
+    } else {
+        console.log(`${pointer}: ${codes[pointer]} | a = ${codes[pointer]}`);
+    }
+    
+    pointer++;
+    const b = modes[1] ? codes[pointer] : codes[codes[pointer]];
+    if (modes[1] === 0) {
+        console.log(`${pointer}: ${codes[pointer]} | b = codes[${codes[pointer]}] = ${b}`);
+    } else {
+        console.log(`${pointer}: ${codes[pointer]} | b = ${codes[pointer]}`);
+    }
+
+    pointer++;
+    codes[codes[pointer]] = a === b ? 1 : 0;
+    console.log(`${pointer}: ${codes[pointer]} | store a === b = ${a === b} in ${codes[pointer]}`);
+    return true;
 }
 
 while (true) {
@@ -94,6 +182,9 @@ while (true) {
     const ret = instruction(modes);
     if (ret === false) {
         break;
+    } else if (ret === true) {
+        pointer++;
+    } else {
+        pointer = ret;
     }
-    pointer++;
 }
