a9bf26b72c09bec948fc9cb3be0ca9113ca07a81
Tim <tmeier42+github@gmail.com>
2019-12-06 17:18:24 +0200

    part 2 wip

--- day 6/solution.js
+++ day 6/solution.js
@@ -3,7 +3,7 @@ const { performance } = require('perf_hooks');
 
 const input = fs.readFileSync('./input.txt', { encoding: 'utf8'});
 
-const orbits = input.split('\n');
+const orbits = input.split(/\r?\n/);
 
 let tree = new Map();
 
@@ -13,12 +13,14 @@ for (let orbit of orbits) {
         tree.set(center, {
             label: center,
             satellites: [],
+            depth: 0,
         });
     }
     if (!tree.has(satellite)) {
         tree.set(satellite, {
             label: satellite,
             satellites: [],
+            depth: 0,
         });
     }
     tree.get(center).satellites.push(tree.get(satellite));
@@ -27,11 +29,33 @@ for (let orbit of orbits) {
 function treeverse (center, startDepth = 0) {
     let totalDepth = startDepth;
     for (let satellite of center.satellites) {
+        satellite.depth = startDepth + 1;
         totalDepth += treeverse(satellite, startDepth + 1);
     }
     return totalDepth
 }
 
+function search (center) {
+    let children = [...center.satellites];
+    let descendants = [];
+    console.log(children.length);
+    for (let satellite of children) {
+        console.log(satellite.label, [...satellite.satellites]);
+        let result = search(satellite);
+        if (typeof result !== 'array') {
+            return result;
+        }
+        console.log('res ' + result);
+        descendants.push(result);
+    }
+    children = [...children, ...descendants];
+    if (children.some(s => s.label === 'YOU') && children.some(s => s.label === 'SAN')) {
+        return children.find(s => s.label === 'YOU').depth + children.find(s => s.label === 'SAN').depth - 2 * center.depth;
+    }
+    return [];
+}
+
 let startTime = performance.now();
 console.log(treeverse(tree.get('COM')));
+console.log(search(tree.get('COM')));
 console.log(performance.now() - startTime);
