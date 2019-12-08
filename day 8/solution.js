154c086ed837bf3ab5648f778cef1d254ec34ee3
Timmeh42 <tmeier42@gmail.com>
2019-12-08 11:13:48 +0200

    part 2

--- day 8/solution.js
+++ day 8/solution.js
@@ -13,11 +13,25 @@ for (let i = 0; i < input.length; i += layerSize) {
     layers.push(input.slice(i, i + layerSize));
 }
 
-console.log(layers);
-
 let layerDigits = layers.map(l => [l.replace(/(1|2)*/g, '').length, l.replace(/(0|2)*/g, '').length, l.replace(/(0|1)*/g, '').length]);
-
-console.log(layerDigits);
 
 const fewestZerosLayer = layerDigits.reduce((cur, nex) => cur = nex[0] < cur[0] ? nex : cur);
 console.log(`Layer with fewest zeroes: ${fewestZerosLayer} - puzzle output: ${fewestZerosLayer[1] * fewestZerosLayer[2]}`);
+
+
+let pixels = '';
+for (let x = 0; x < layerSize; x++) {
+    for (let z = 0; z < layerCount; z++) {
+        if (input[x + z*layerSize] !== '2') {
+            pixels += input[x + z*layerSize];
+            break;
+        }
+    }
+}
+
+let image = [];
+for (let i = 0; i < layerSize; i += width) {
+    image.push(pixels.slice(i, i+width));
+}
+
+console.log(image);
