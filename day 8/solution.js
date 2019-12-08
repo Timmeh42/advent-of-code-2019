971b16984900791b2cf87efe3c3478b0ace1a8cf
Tim <tmeier42+github@gmail.com>
2019-12-08 20:48:10 +0200

    fassssterrr

--- day 8/solution.js
+++ day 8/solution.js
@@ -1,16 +1,17 @@
 "use strict";
-const { slices, range } = require('../helpers');
+const { slices } = require('../helpers');
 
 module.exports = function ( input ) {
     const [ width, height ] = [ 25, 6 ];
     const layerSize = width * height;
     const layerCount = Math.floor(input.length/layerSize);
-    const layerDigits = slices(input, layerSize).map(l => [l.match(/0/g).length, l.match(/1/g).length, l.match(/2/g).length]);
-    const fewestZerosLayer = layerDigits.reduce((cur, nex) => cur = nex[0] < cur[0] ? nex : cur);
+    const [ reg0, reg1, reg2 ] = [ RegExp(/0/g), RegExp(/1/g), RegExp(/2/g) ]
+    const layerDigits = slices(input, layerSize).map(l => [l.match(reg0).length, l]);
+    const fewestZerosLayer = layerDigits.reduce((cur, nex) => nex[0] < cur[0] ? nex : cur);
     
     let pixels = '';
-    for (const x of range(layerSize)) {
-        for (const z of range(layerCount)) {
+    for (let x = 0; x < layerSize; x++) {
+        for (let z = 0; z < layerCount; z++) {
             if (input[x + z*layerSize] !== '2') {
                 pixels += input[x + z*layerSize];
                 break;
@@ -18,11 +19,11 @@ module.exports = function ( input ) {
         }
     }
     
-    pixels = pixels.replace(/1/g, '#').replace(/0/g, ' ');
+    pixels = pixels.replace(/0|1/g, (match) => match === '0' ? ' ' : '#');
     const image = slices(pixels, width).join('\n');
     
     return [
-        fewestZerosLayer[1] * fewestZerosLayer[2],
+        fewestZerosLayer[1].match(reg1).length * fewestZerosLayer[1].match(reg2).length,
         image,
     ];
 }
