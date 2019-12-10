7082ab54a112075c7caa56c59b05219843333bef
Timmeh42 <tmeier42@gmail.com>
2019-12-10 10:26:34 +0200

    part 2 wip

--- day 10/solution.js
+++ day 10/solution.js
@@ -15,22 +15,52 @@ module.exports = function (input) {
         }
     }
 
-    let base = {eyes: new Set()};
+    let base = {angCount: 0};
     for (let me of asteroids) {
-        const [x, y] = [me.x, me.y];
-        me.eyes = new Set();
+        let angs = {};
         for (let asteroid of asteroids) {
+            if (asteroid === me) {
+                continue;
+            }
             let [rx, ry] = [asteroid.x - me.x, asteroid.y - me.y];
             let ang = Math.atan2(ry, rx);
-            me.eyes.add(ang);
+            let dis = Math.abs(rx) + Math.abs(ry);
+            if (!angs.hasOwnProperty(ang)) {
+                angs[ang] = [];
+            }
+            asteroid.dis = dis;
+            angs[ang].push(asteroid);
         }
-        if (base.eyes.size < me.eyes.size) {
+        if (base.angCount < Object.keys(angs).length) {
+            me.angs = angs;
+            me.angCount = Object.keys(angs).length;
             base = me;
         }
     }
 
+    let UP = Math.atan2(1, 0);
+    let victim = 0;
+    let angsQueue = Object.keys(base.angs).sort((a, b) => a - b);
+    console.log(angsQueue);
+    while (angsQueue[0] < UP) {
+        angsQueue.push(angsQueue.unshift());
+    }
+    console.log('lasor');
+    while (victim < 200){
+        let ang = base.angs[angsQueue[victim % angsQueue.length]];
+        let nextVictim = ang.sort((a, b) => a.dis - b.dis).shift();
+        console.log(victim, nextVictim.dis);
+        victim++;
+    }
+
+
+
+
+
+
+
     return [
-        base.eyes.size,
+        base.angCount,
         '',
     ]
 }
