d38e085ddd992b561ec60b71d7d734bbfbeb0774
Timmeh42 <tmeier42@gmail.com>
2019-12-10 19:17:09 +0200

    part 2

--- day 10/solution.js
+++ day 10/solution.js
@@ -15,23 +15,54 @@ module.exports = function (input) {
         }
     }
 
-    let base = {eyes: new Set()};
+    let base = null;
     for (let me of asteroids) {
-        const [x, y] = [me.x, me.y];
-        me.eyes = new Set();
+        me.angles = new Map();
         for (let asteroid of asteroids) {
-            let [rx, ry] = [asteroid.x - me.x, asteroid.y - me.y];
-            let ang = Math.atan2(ry, rx);
-            me.eyes.add(ang);
+            if (asteroid === me) {
+                continue;
+            }
+            const [dx, dy] = [asteroid.x - me.x, asteroid.y - me.y];
+            const ang = Math.atan2(-dx, dy) + 2 * Math.PI;
+            const dis = Math.hypot(dx, dy);
+            if (!me.angles.has(ang)) {
+                me.angles.set(ang, []);
+            }
+            me.angles.get(ang).push({
+                ang,
+                dis,
+                asteroid,
+            });
         }
-        if (base.eyes.size < me.eyes.size) {
+        if (base === null || base.angles.size < me.angles.size) {
             base = me;
         }
     }
+    const part1 = base.angles.size;
+
+    for (let asteroidList of base.angles.values()) {
+        asteroidList.sort((a, b) => a.dis - b.dis);
+    }
+
+    let listQueue = [...base.angles.keys()].sort((a, b) => a - b).map(n => base.angles.get(n));
+
+    let asteroidQueue = [];
+    while (listQueue.length !== 0) {
+        const activeAngle = listQueue.shift();
+        asteroidQueue.push(activeAngle.shift());
+        if (activeAngle.length !== 0) {
+            listQueue.push(activeAngle);
+        }
+    }
+
+    const a200 = asteroidQueue[199].asteroid;
+    const part2 = a200.x * 100 + a200.y;
+
+
 
     return [
-        base.eyes.size,
-        '',
+        part1,
+        part2,
     ]
 }
 
